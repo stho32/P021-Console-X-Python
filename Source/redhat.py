d3d430ed7db1c944faccd315954fad6f65d83766
stho32 <stho32github@gmail.com>
2022-01-16 13:20:45 +0100

    basic filtering is working now! :)

--- Source/redhat.py
+++ Source/redhat.py
@@ -1,4 +1,3 @@
-from email.header import decode_header
 import os
 import json
 from imap_tools import MailBox, AND
@@ -17,13 +16,17 @@ else :
     username = input("username: ")
     password = getpass.getpass("password: ")
 
-# get list of email subjects from INBOX folder
-with MailBox(server).login(username, password) as mailbox:
-    subjects = [msg.subject for msg in mailbox.fetch()]
-
 # get list of email subjects from INBOX folder - equivalent verbose version
 mailbox = MailBox(server)
 mailbox.login(username, password, initial_folder='INBOX')  # or mailbox.folder.set instead 3d arg
-subjects = [msg.subject for msg in mailbox.fetch(AND(all=True))]
-print(subjects)
+
+messages = mailbox.fetch(AND(all=True))
+
+for message in messages:
+    print(f"- {message.from_} : {message.subject}")
+    if not message.from_:
+        print("   -> this is spam, moving it")
+        mailbox.move([message.uid], "INBOX/MySpam")
+
 mailbox.logout()
+
