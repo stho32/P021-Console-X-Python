360b6a955237e03f87a4e8d5ed3273745ab6278f
stho32 <stho32github@gmail.com>
2022-03-25 19:19:24 +0100

    preps for keyring

--- Source/mailfilter.py
+++ Source/mailfilter.py
@@ -1,32 +1,32 @@
 import os
 import json
 from imap_tools import MailBox, AND
-import getpass
+import keyring
+import argh
 
-config_file = "./Source/config.json"
+application_id = "mailfilter"
 
-if os.path.isfile("./Source/config.json"):
-    with open('./Source/config.json') as f:
-        data = json.load(f)
-        server = data["server"]
-        username = data["username"]
-        password = getpass.getpass("password: ")
-else:
-    server = input("server: ")
-    username = input("username: ")
-    password = getpass.getpass("password: ")
+access_data = keyring.get_password(application_id, "configuration")
+
+
+print(access_data)
+
+# server = data["server"]
+# username = data["username"]
+# password = getpass.getpass("password: ")
+
 
 # get list of email subjects from INBOX folder - equivalent verbose version
-mailbox = MailBox(server)
-mailbox.login(username, password, initial_folder='INBOX')  # or mailbox.folder.set instead 3d arg
+# mailbox = MailBox(server)
+# mailbox.login(username, password, initial_folder='INBOX')  # or mailbox.folder.set instead 3d arg
 
-messages = mailbox.fetch(AND(all=True))
+# messages = mailbox.fetch(AND(all=True))
 
-for message in messages:
-    print(f"- {message.from_} : {message.subject}")
+# for message in messages:
+#     print(f"- {message.from_} : {message.subject}")
 
-    if not message.from_:
-        print("   -> this is spam, moving it")
-        mailbox.move([message.uid], "INBOX/MySpam")
+#     if not message.from_:
+#         print("   -> this is spam, moving it")
+#         mailbox.move([message.uid], "INBOX/MySpam")
 
-mailbox.logout()
+# mailbox.logout()
